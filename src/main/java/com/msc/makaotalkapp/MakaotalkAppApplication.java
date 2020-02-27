6abd3989de22c602d4cc56d2203f27d0df7211b6
moon_ <m3252@naver.com>
2020-02-27 18:47:06 +0900

    first commit

---
+++ src/main/java/com/msc/makaotalkapp/MakaotalkAppApplication.java
@@ -0,0 +1,13 @@
+package com.msc.makaotalkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakaotalkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakaotalkAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msc/makaotalkapp/MakaotalkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.makaotalkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakaotalkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

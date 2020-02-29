2b10a5df6d107449b65d8a8fdac16203fc3c0ea0
moon_ <m3252@naver.com>
2020-02-29 19:51:48 +0900

    JPA MySql Test

--- src/main/java/com/msc/makaotalkapp/controller/TestController.java
+++ src/main/java/com/msc/makaotalkapp/controller/TestController.java
@@ -1,12 +1,40 @@
 package com.msc.makaotalkapp.controller;
 
+import com.msc.makaotalkapp.entity.Member;
+import com.msc.makaotalkapp.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+//@RestController는 모든 메소드에 @ResponseBody를 적용해준다.
+@RestController
 public class TestController {
-    @GetMapping("/home")
-    public void home(){
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/readAll")
+    public Optional readAll(String id) {
+        return memberRepository.findById(id);
+    }
+
+
+    @GetMapping("/create")
+    public void create(Member member) {
+        memberRepository.save(member);
+    }
+
+    @GetMapping("/delete")
+    public void delete(String id) {
+        memberRepository.deleteById(id);
     }
 }
